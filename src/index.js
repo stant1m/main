d97cc4c21bedf9990469eb2f85b9762a6a010775
stant1m <115929692+stant1m@users.noreply.github.com>
2022-11-03 19:32:53 +0300

    Final

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import "./scss/style.scss";
+import "./scss/fonts.scss";
+import "./js/swiper.js";
+import "./js/showallbtn.js";
+import "./js/Burger-menu-btn.js";
+import "./js/Micro-menu-active.js";
+import "./js/action-btns.js";
+// Img brands
+import './img/brands/lenovo.svg';
+import "./img/brands/samsung.svg";
+import "./img/brands/apple.svg";
+import './img/brands/viewsonic.svg';
+import "./img/brands/bosch.svg";
+import "./img/brands/hp.svg";
+import "./img/brands/acer.svg";
+import "./img/brands/sony.svg";
+import "./img/brands/lenovo.svg";
+import "./img/brands/samsung.svg";
+import "./img/brands/apple.svg";
+
+// Img buttons
+import "./img/circle-icons/x.svg";
+import "./img/circle-icons/burger.svg";
+import "./img/circle-icons/call.svg";
+import "./img/circle-icons/chat.svg";
+import "./img/circle-icons/profile.svg";
+import "./img/circle-icons/repair.svg";
+import "./img/circle-icons/search.svg";
+import "./img/circle-icons/checkstatus.svg";
+
+// Img helpers
+import "./img/helpers/divider.svg";
+import "./img/helpers/highlight.svg";
+import "./img/helpers/arrow-down.svg";
+import "./img/helpers/arrow-up.svg";
+import "./img/helpers/more.svg";
+import "./img/helpers/go.svg";
+import "./img/helpers/button.svg";
+import "./img/helpers/dropdown.svg";
+import "./img/helpers/expand.svg";
+import "./img/helpers/Group 10.svg";
+
+// Img logo
+import "./img/logo/logo.svg";
+
+// Img src
+import "./img/";
